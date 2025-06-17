ac7a80b4a92a467b986419ec3e6c4b43e00c8427
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-06-17 10:10:35 +0200

    Clase ConexionDB completada

--- src/util/ConexionDB.java
+++ src/util/ConexionDB.java
@@ -1,5 +1,32 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.*;
+
 public class ConexionDB {
+
+	private static final String URL = "jdbc:mysql://localhost:3306/hotel?useSSL=false&serverTimezone=UTC";
+	private static final String USER = "root";
+	private static final String PASSWORD = "";
+
+	public static Connection obtenerConexion() {
+
+		Connection conexion = null;
+
+		try {
+			conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+			System.out.println("Conexion exitosa con la base de datos.");
+
+		} catch (SQLException e) {
+
+			System.out.println("No se ha podido conectar con la base de datos.");
+			e.printStackTrace();
+
+		}
+		return conexion;
+
+	}
 
 }
